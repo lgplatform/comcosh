bfe81bcc7935f9b77273fb846b6630d4fd7011c6
msjeon529 <minssogi@line.games>
2019-06-07 18:28:34 +0900

    + contributor check test

--- src/main/java/com/example/common/controller/Login.java
+++ src/main/java/com/example/common/controller/Login.java
@@ -1,5 +1,6 @@
 package com.example.common.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/v1/basic")
+@Slf4j
 public class Login {
 
 	@GetMapping("/login")
 	public String loginPage(Model model){
-
+		log.info("### login start ###");
 		return "login";
 	}
 
