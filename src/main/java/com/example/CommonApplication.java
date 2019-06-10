af2ce5677e5e9f46e63f08237f4ae8fa9dbb7aaa
Mingsicogi <codedoctor119@gmail.com>
2019-06-11 04:06:51 +0900

    + line login

--- src/main/java/com/example/CommonApplication.java
+++ src/main/java/com/example/CommonApplication.java
@@ -3,7 +3,11 @@ package com.example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(value = {
+        "classpath:common.properties"
+}, encoding = "UTF-8")
 @SpringBootApplication
 @Slf4j
 public class CommonApplication {
--- src/main/java/com/example/common/controller/Login.java
+++ src/main/java/com/example/common/controller/Login.java
@@ -1,20 +1,45 @@
 package com.example.common.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/v1/basic")
 @Slf4j
 public class Login {
 
+	@Value("${line.chanel.id}")
+	private String lineChanelId;
+
+	@Value("${line.me.login.url.format}")
+	private String lineMeUrlFormat;
+
+	@Value("${domain}")
+	private String domain;
+
+	@Value("${server.port}")
+	private String port;
+
+	/**
+	 * line login
+	 *
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/login")
 	public String loginPage(Model model){
 
-		return "login";
+		String loginRedirectUrl = lineMeUrlFormat;
+
+		loginRedirectUrl = String.format(loginRedirectUrl, lineChanelId, domain + ":" + port + "/v1/basic/welcome", Math.random());
+
+		return "redirect:" + loginRedirectUrl;
 	}
 
 }
